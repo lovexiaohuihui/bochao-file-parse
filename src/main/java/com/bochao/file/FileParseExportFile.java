3fb24bbe69f838cb517abd2a81859731679fbc23
“wujunjie” <1035626172@qq.com>
2022-01-26 10:00:32 +0800

    excel

--- src/main/java/com/bochao/file/FileParseExportFile.java
+++ src/main/java/com/bochao/file/FileParseExportFile.java
@@ -9,6 +9,7 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 import org.springframework.util.StringUtils;
+
 import java.io.*;
 import java.nio.charset.Charset;
 import java.util.*;
@@ -17,10 +18,13 @@ import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 /**
- * 模型文件数据转换 文件合并导出
+ * 模型文件数据转换 导出单个文件
  * */
 @Log4j2
-public class FileParse {
+public class FileParseExportFile {
+
+    public static final String SOURCE_PATH = "C:/file/source2.zip";
+
     public static void main(String[] args) {
         // 每行信息
         Row row = null;
@@ -65,16 +69,19 @@ public class FileParse {
             log.info("解析编码规范文件结束");
             // 二. 夺取压缩文件模型名称数据
             // 1- 获取压缩文件
-            String path = "C:/file/荣信阀塔.zip";
+            String path = SOURCE_PATH;
             ZipFile zipFile = new ZipFile(path, Charset.forName("GBK"));
             InputStream in = new BufferedInputStream(new FileInputStream(path));
             ZipInputStream zin = new ZipInputStream(in, Charset.forName("GBK"));
             ZipEntry ze;
             // 保存压缩文件内容list
             List<String> modelNameList = new ArrayList<>();
+            FileOutputStream outputStream = null;
             while ((ze = zin.getNextEntry()) != null) {
                 if (!ze.isDirectory()) {
+                    modelNameList.clear();
                     long size = ze.getSize();
+                    String fileName = ze.getName().split("\\/")[1].split("\\.")[0];
                     if (size > 0) {
                         BufferedReader br = new BufferedReader(new InputStreamReader(zipFile.getInputStream(ze), Charset.forName("gbk")));
                         String line;
@@ -84,48 +91,51 @@ public class FileParse {
                         }
                         br.close();
                     }
+                    zin.closeEntry();
+                    // 三. 导出编码信息数据
+                    // 1- 创建一个工作表sheet
+                    HSSFWorkbook writeWorkbook = new HSSFWorkbook();
+                    // 2 创建HSSFSheet对象
+                    HSSFSheet writeSheet = writeWorkbook.createSheet("sheet0");
+                    // 3 设置默认行宽
+                    writeSheet.setDefaultColumnWidth(20);
+                    // 4 设置单元格样式
+                    HSSFCellStyle cellStyle = HSSFWorkbookUtil.getCellStyle(writeWorkbook);
+                    // 5 表头信息处理
+                    // 创建HSSFRow对象，创建表头信息行
+                    HSSFRow wirteRow = writeSheet.createRow(0);
+                    // 表头信息
+                    String[] data = {"区域", "设备", "部件", "编码", "模型名称"};
+                    // 将表头信息转换为表格数据保存
+                    HSSFWorkbookUtil.getCells(data, wirteRow, writeWorkbook, cellStyle);
+                    // 6 将模型数据转换为表格数据导出
+                    Integer num = 0;
+                    for (String modelName : modelNameList) {
+                        // 从map中获取对应的设备数据
+                        // 获取最后一个_前的数据
+                        int index = modelName.lastIndexOf("_");
+                        String leftName  = modelName.substring(0, index);
+                        String rightName  = modelName.substring(index,  modelName.length());
+                        DeviceInfo writeInfo = map.get(leftName);
+                        if (writeInfo != null) {
+                            num = num + 1;
+                            wirteRow = writeSheet.createRow(num);
+                            String[] measureData = {writeInfo.getArea(), writeInfo.getDeviceName(), writeInfo.getParts(),writeInfo.getCode() + rightName, writeInfo.getModelName() + rightName};
+                            HSSFWorkbookUtil.getCells(measureData, wirteRow, writeWorkbook, cellStyle);
+                        }
+                    }
+                    // 5 输出Excel文件
+                    // 打开文件流
+                    File file = new File("C:file/export2/" + fileName + ".csv");
+                    file.createNewFile();
+                   outputStream = new FileOutputStream(file);
+                    writeWorkbook.write(outputStream);
+                    outputStream.flush();
                 }
             }
-            zin.closeEntry();
-            // 三. 导出编码信息数据
-            // 1- 创建一个工作表sheet
-            HSSFWorkbook writeWorkbook = new HSSFWorkbook();
-            // 2 创建HSSFSheet对象
-            HSSFSheet writeSheet = writeWorkbook.createSheet("sheet0");
-            // 3 设置默认行宽
-            writeSheet.setDefaultColumnWidth(20);
-            // 4 设置单元格样式
-            HSSFCellStyle cellStyle = HSSFWorkbookUtil.getCellStyle(writeWorkbook);
-            // 5 表头信息处理
-            // 创建HSSFRow对象，创建表头信息行
-            HSSFRow wirteRow = writeSheet.createRow(0);
-            // 表头信息
-            String[] data = {"区域", "设备", "部件", "编码", "模型名称"};
-            // 将表头信息转换为表格数据保存
-            HSSFWorkbookUtil.getCells(data, wirteRow, writeWorkbook, cellStyle);
-            // 6 将模型数据转换为表格数据导出
-            Integer num = 0;
-            for (String modelName : modelNameList) {
-                // 从map中获取对应的设备数据
-                // 获取最后一个_前的数据
-                int index = modelName.lastIndexOf("_");
-                String leftName  = modelName.substring(0, index);
-                String rightName  = modelName.substring(index,  modelName.length());
-                DeviceInfo writeInfo = map.get(leftName);
-                if (writeInfo != null) {
-                    num = num + 1;
-                    wirteRow = writeSheet.createRow(num);
-                    String[] measureData = {writeInfo.getArea(), writeInfo.getDeviceName(), writeInfo.getParts(),writeInfo.getCode() + rightName, writeInfo.getModelName() + rightName};
-                    HSSFWorkbookUtil.getCells(measureData, wirteRow, writeWorkbook, cellStyle);
-                }
+            if (outputStream != null) {
+                outputStream.close();
             }
-            // 5 输出Excel文件
-            // 打开文件流
-            File file = new File("C:file/编码规范" + new Date().getTime() + ".csv");
-            file.createNewFile();
-            FileOutputStream outputStream = new FileOutputStream(file);
-            writeWorkbook.write(outputStream);
-            outputStream.close();
         } catch (Exception e) {
             log.info("失败" + row);
             e.printStackTrace();
